a3eb504771bd005dc1738d9cdc96be571dbba3e7
Wiktor <wiktor.vip@gmail.com>
2024-02-12 12:02:38 +0100

    update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 DB_Host = os.environ.get('DB_Host') or "localhost"
 DB_Database = os.environ.get('DB_Database') or "mysql"
 DB_User = os.environ.get('DB_User') or "root"
-DB_Password = os.environ.get('DB_Password') or "paswrd"
+DB_Password = os.environ.get('DB_Password') or "password"
 
 @app.route("/")
 def main():
